3bf6c6494da662359f5f805be3629ddb9a53a1c9
Jonathan Tromp <jonatromp45@gmail.com>
2024-11-19 14:01:27 +0000

    completed basic layout for festival recommendations page

--- lib/recommendations.dart
+++ lib/recommendations.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:url_launcher/url_launcher.dart';
 
-// RichText(
-//   text: TextSpan(
-//     children: [
-//       TextSpan(
-//         text: 'Here is clickable',
-//         style: TextStyle(color: Colors.black),
-//       ),
-//       TextSeparator(widget which represents an inline link),
-//       TextSpan(
-//         text: 'Cutsomise this part as needed',
-//         style: TextStyle(color: Colors.black),
-//       ),
-//     ],),
-// )
-
 class Recommendations extends StatelessWidget {
   const Recommendations({super.key});
 
@@ -23,11 +8,75 @@ class Recommendations extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Row(
+        body: GridView.count(
+          crossAxisCount: 1,
           children: [
-            const Text(
-              'Top 3 Festivals for You',
-              style: TextStyle(fontSize: 30),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 12, 145, 17),
+                    border: Border.all(width: 8),
+                    borderRadius: BorderRadius.circular(20)),
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(10.0),
+                child: const Text(
+                  'Top 3 Festivals for You...',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 12, 145, 17),
+                    border: Border.all(width: 8),
+                    borderRadius: BorderRadius.circular(20)),
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(10.0),
+                child: const Text(
+                  'Top 3 Festivals Your Favourite Artists Are At...',
+                  style: TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold, height: 1.5),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 12, 145, 17),
+                    border: Border.all(width: 8),
+                    borderRadius: BorderRadius.circular(20)),
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(10.0),
+                child: const Text(
+                  'Top 3 Festivals Including Your Favourite Genre...',
+                  style: TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold, height: 1.5),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 12, 145, 17),
+                    border: Border.all(width: 8),
+                    borderRadius: BorderRadius.circular(20)),
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(10.0),
+                child: const Text(
+                  'Top 3 Festivals that are a bit different...',
+                  style: TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold, height: 1.5),
+                ),
+              ),
             ),
           ],
         ));
