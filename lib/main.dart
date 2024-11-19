14b7f3cb68de99adcf8c4fc63bbcb63f5858f2e5
DanAth95 <daniel_atherton@aol.co.uk>
2024-11-19 11:00:41 +0000

    added new login button

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// import 'package:oauth2_client/spotify_oauth2_client.dart';
-// import 'package:oauth2_client/access_token_response.dart';
+import 'package:oauth2_client/spotify_oauth2_client.dart';
+import 'package:oauth2_client/access_token_response.dart';
 import 'settings.dart';
-import 'package:musiCAL/pages/spotify_auth_page.dart';
+// import 'package:musical/pages/spotify_auth_page.dart';
 import 'defaulttab.dart';
 // import 'package:http/http.dart' as http;
-import 'package:musiCAL/firebase_options.dart';
+import 'package:musical/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,53 +15,23 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  // var Access_Token;
-  // var Refresh_Token;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  // Future<void> remoteService() async {
-  //   final String clientId = '809e9a055f604342a727aa3961f343d2';
-  //   final String clientSecret = '6aa9ae2264094650a6af77b3eef14903';
-  //   AccessTokenResponse? accessToken;
-  //   SpotifyOAuth2Client client = SpotifyOAuth2Client(
-  //     customUriScheme: 'musiCAL.app',
-  //     //Must correspond to the AndroidManifest's "android:scheme" attribute
-  //     redirectUri:
-  //         'musiCAL.app://callback', //Can be any URI, but the scheme part must correspond to the customeUriScheme
-  //   );
-  //   var authResp =
-  //       await client.requestAuthorization(clientId: clientId, customParams: {
-  //     'show_dialog': 'true'
-  //   }, scopes: [
-  //     'user-read-private',
-  //     'user-read-playback-state',
-  //     'user-modify-playback-state',
-  //     'user-read-currently-playing',
-  //     'user-read-email'
-  //   ]);
-  //   var authCode = authResp.code;
-
-  //   accessToken = await client.requestAccessToken(
-  //       code: authCode.toString(),
-  //       clientId: clientId,
-  //       clientSecret: clientSecret);
-
-  //   // Global variables
-  //   Access_Token = accessToken.accessToken;
-  //   Refresh_Token = accessToken.refreshToken;
-  // }
-
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        initialRoute: '/login',
+        home: _SpotifyLoginState(),
         routes: {
-          '/login': (context) => SpotifyAuthPage(onCodeReceived: (c) {}),
+          // '/login': (context) => _SpotifyLoginState(),
           '/calendar': (context) => Navbar2(),
           //'/events': (context) => Navbar2(),
           //   '/festical': (context) => festical(),
@@ -73,5 +43,62 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF1F2421)),
           useMaterial3: true,
         ));
+  }
+}
+
+class _SpotifyLoginState extends StatefulWidget {
+  const _SpotifyLoginState({super.key});
+
+  @override
+  State<_SpotifyLoginState> createState() => __SpotifyLoginStateState();
+}
+
+class __SpotifyLoginStateState extends State<_SpotifyLoginState> {
+  String? accessToken = '';
+  String? refreshToken = '';
+
+  Future<void> remoteService() async {
+    final String clientId = '809e9a055f604342a727aa3961f343d2';
+    final String clientSecret = '6aa9ae2264094650a6af77b3eef14903';
+    AccessTokenResponse? accessT;
+    SpotifyOAuth2Client client = SpotifyOAuth2Client(
+      customUriScheme: 'http://localhost:50511/',
+      redirectUri: 'http://localhost:50511/',
+    );
+    var authResp =
+        await client.requestAuthorization(clientId: clientId, scopes: [
+      'user-read-private',
+      'user-read-playback-state',
+      'user-modify-playback-state',
+      'user-read-currently-playing',
+      'user-read-email'
+    ]);
+    var authCode = authResp.code;
+
+    accessT = await client.requestAccessToken(
+        code: authCode.toString(),
+        clientId: clientId,
+        clientSecret: clientSecret);
+
+    // Global variables
+    accessToken = accessT.accessToken;
+    refreshToken = accessT.refreshToken;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+          child: Column(children: [
+        ElevatedButton(
+            onPressed: () {
+              remoteService();
+            },
+            child: Text('login'))
+      ])),
+    );
   }
 }
