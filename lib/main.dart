315a1d98185dceea30137d5cf6f7052e824cb316
Jonathan Tromp <jonatromp45@gmail.com>
2024-11-20 16:37:52 +0000

    imported navigation to main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'bottomnavbar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
